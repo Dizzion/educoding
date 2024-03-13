66f7ceece50f1f389cfdfbf51df1588efa1d22ce
Dizzion <gofrodude_197@hotmail.com>
2024-03-12 19:54:53 -0700

    update for auth for general use

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,32 +1,25 @@
-import { useRouter } from 'next/router';
-import { FormEvent } from 'react';
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "../../lib/actions";
 
 export default function Login() {
-    const router = useRouter();
-
-    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const email = formData.get('email');
-        const password = formData.get('password');
-
-        const response = await fetch('api/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password }),
-        })
-
-        if (response.ok) {
-            router.push('/Dashboard');
-        } else {
-            // Handle errors
-        }
-    }
+    const [errorMessage, dispatch] = useFormState(authenticate, undefined)
+ 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type='email' name='email' placeholder='Email' required />
-            <input type='password' name='password' placeholder='Password' required />
-            <button type='submit'>Login</button>
-        </form>
+      <form action={dispatch}>
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <div>{errorMessage && <p>{errorMessage}</p>}</div>
+        <LoginButton />
+      </form>
     )
-}
+  }
+   
+  function LoginButton() {
+    const { pending } = useFormStatus()
+   
+    return (
+      <button aria-disabled={pending} type="submit">
+        Login
+      </button>
+    )
+  }
