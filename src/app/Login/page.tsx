27fdf7a2bbd4a58689d710ab431e476303b0da21
Dizzion <gofrodude_197@hotmail.com>
2024-03-12 19:44:36 -0700

    basic login page set

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,6 +1,32 @@
+import { useRouter } from 'next/router';
+import { FormEvent } from 'react';
 
 export default function Login() {
+    const router = useRouter();
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        const response = await fetch('api/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+        })
+
+        if (response.ok) {
+            router.push('/Dashboard');
+        } else {
+            // Handle errors
+        }
+    }
     return (
-        <div></div>
+        <form onSubmit={handleSubmit}>
+            <input type='email' name='email' placeholder='Email' required />
+            <input type='password' name='password' placeholder='Password' required />
+            <button type='submit'>Login</button>
+        </form>
     )
 }
