db5b008481132d6adc54a3c333c3a5c90244c2e4
Dizzion <gofrodude_197@hotmail.com>
2024-10-27 20:03:03 -0700

    fix to ip address

--- src/utils/dbActions.ts
+++ src/utils/dbActions.ts
@@ -1,7 +1,7 @@
 "use server";
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.0:8090");
+const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function createAnswer(
   codeAnswer: string,
