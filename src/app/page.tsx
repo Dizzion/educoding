cc8445b3e56b661b3f91a7794ac3d5c87a2bab33
Dizzion <gofrodude_197@hotmail.com>
2024-10-29 20:49:39 -0700

    landing page work and svg adds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
-    <main
-      className="w-full
-    h-screen
-    bg-gradient-to-r
-    from-emerald-100
-    via-slate-100
-    to-blue-100
-    background-animate"
-    >
-      <div>hello world</div>
+    <main className="flex justify-center">
+      <div className="pt-10">stuff goes here</div>
     </main>
   );
 }
