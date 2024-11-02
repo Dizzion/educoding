e23ae17391fb4e3d85acccd7699c243c4b3ec257
Dizzion <gofrodude_197@hotmail.com>
2024-11-02 14:08:50 -0700

    update header to exist on the dasboard will have second header for main

--- src/app/Dashboard/layout.tsx
+++ src/app/Dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 
 export default function DashboardLayout({
   children,
@@ -6,6 +7,7 @@ export default function DashboardLayout({
 }>) {
   return (
     <>
+      <Header/>
       {children}
     </>
   );
