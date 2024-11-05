4b0977b012d6608b6a9b6dd01cc841f14199a2f6
Dizzion <gofrodude_197@hotmail.com>
2024-11-04 23:44:24 -0800

    update all the text to relivant text for site

--- src/components/large-testimonial.tsx
+++ src/components/large-testimonial.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import TestimonialImg from "../../../../../../../../../../../../../../public/images/large-testimonial.jpg";
+import TestimonialImg from "/public/images/large-testimonial.jpg";
 
 export default function LargeTestimonial() {
   return (
@@ -30,16 +30,16 @@ export default function LargeTestimonial() {
               />
             </div>
             <p className="text-2xl font-bold text-gray-900">
-              “Simple has simplified my life in more ways than one. From
-              managing my sites to{" "}
-              <em className="italic text-gray-500">keeping track of tasks</em>,
-              it's become my go-to tool for everything.”
+              “EDUCoding helped me work towards my goal of becoming a software engineer.
+              From working with data to{" "}
+              <em className="italic text-gray-500">understanding the language I'm working in</em>,
+              it's become my go-to tool for learning concepts.”
             </p>
             <div className="text-sm font-medium text-gray-500">
-              <span className="text-gray-700">Mary Sullivan</span>{" "}
+              <span className="text-gray-700">Aiden Shaver</span>{" "}
               <span className="text-gray-400">/</span>{" "}
               <a className="text-blue-500" href="#0">
-                CTO at Microsoft
+                High School Student
               </a>
             </div>
           </div>
